6497616568f07c51e7247cf992a4791deaee73e2
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-05 00:23:56 +0200

    adding buttons

--- src/App.js
+++ src/App.js
@@ -13,7 +13,13 @@ const App = () => {
       <h1>Calculator</h1>
       <div className='display'>{input || '0'}</div>
       <div className='buttons'>
-        
+        {["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "C", "+", "="].map(
+          (value) => (
+            <button key={value} onClick={() => handleButtonClick(value)}>
+              {value}
+            </button>
+          )
+        )}
       </div>
     </div>
   );
